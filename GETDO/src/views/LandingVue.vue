6d978e1046788d8dd81bdea8b0a61ed5c816d4ae
Nkwe <nkabo074@gmail.com>
2024-06-08 17:38:31 +0100

    massive commit

---
+++ GETDO/src/views/LandingVue.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div class="title-container">
+    <h1>GETDO</h1>
+    <h2>A simple task manager</h2>
+  </div>
+  <div class="button-container"></div>
+</template>
+
+<style scoped>
+</style>
