352d36ab3ddc6567e7bc025dfbfc6235748e7aa6
Nkwe <nkabo074@gmail.com>
2024-06-17 18:14:24 +0100

    massive modifications commit

--- GETDO/src/views/HomeVue.vue
+++ GETDO/src/views/HomeVue.vue
@@ -2,26 +2,41 @@
 
 <template>
   <body class="page-container">
-    <input type="search" class="input-fields" name="search" id="search">
-    <button class="fancyButton">Recherche</button>
+    <div class="task-search">
+      <input type="search" class="input-fields" name="search" id="search" />
+      <button class="fancyButton">Recherche</button>
+      <button class="fancyButton">Crée une nouvelle tâche</button>
+    </div>
     <div class="task">
       <h2 class="task-title">Lorem Task</h2>
       <div class="task-description">
-        <p class="task-description-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint atque blanditiis.</p>
+        <p class="task-description-text">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint atque
+          blanditiis.
+        </p>
       </div>
-      <button class="task-delete base-button">supprimer</button>
-      <button class="task-mod base-button">modifier</button>
-      <button class="task-validation base-button">terminer</button>
-    </div>    
+      <p class="task-duration">25-05-2024 au 29-05-2024</p>
+      <div class="task-buttons">
+        <button class="task-delete base-button">supprimer</button>
+        <button class="task-mod base-button">modifier</button>
+        <button class="task-validation base-button">terminer</button>
+      </div>
+    </div>
   </body>
 </template>
 
 <style scoped>
+.task-search {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
 .base-button {
   border-radius: 10px;
   border-style: none;
   padding: 10px 40px 10px 40px;
-  margin:0 7px 0 7px ;
+  margin: 0 7px 0 7px;
   color: var(--clear-default);
   font-size: 14px;
   font-weight: bold;
@@ -44,8 +59,9 @@
 
 .task-description {
   background-color: #00000069;
-  padding: 10px;
+  padding: 7px;
   border-radius: 10px;
+  font-size: 14px;
 }
 
 .task {
@@ -54,5 +70,14 @@
   padding: 1% 1% 1.5% 1%;
   border-radius: 18px;
   border: 1px solid var(--border-color);
+}
+
+.task-title {
+  margin: 7px;
+}
+
+.task-duration {
+  margin: 2px 0px -10px 0;
+  font-size: 14px;
 }
 </style>
