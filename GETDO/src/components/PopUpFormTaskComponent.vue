276bbcbb69c994b323c1b159d375284a9179765e
NkABO074 <nkabo074@gmail.com>
2024-07-01 11:57:44 +0100

    massive modification

--- GETDO/src/components/PopUpFormTaskComponent.vue
+++ GETDO/src/components/PopUpFormTaskComponent.vue
@@ -1,27 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="popup-content">
-    <form class="popup-content-form">
-      <label for="task_name">Nom de la tâche</label>
-      <input class="generic-input-fields" type="text" name="" id="task_name" />
-      <label for="task_desc">Description de la tâche</label>
-      <textarea name="" class="generic-input-fields" id="task_desc"></textarea>
-
-      <p>priorité de la tâche:</p>
-
-      <label for="light">faible</label>
-      <input type="radio" name="" id="light" />
-      <label for="medium">moyenne</label>
-      <input type="radio" name="" id="medium" />
-
-      <label for="heavy">haute</label>
-      <input type="radio" name="" id="heavy" />
-
-      <label for="phat">élevé</label>
-      <input type="radio" name="" id="phat" />
-    </form>
-  </div>
+  
 </template>
 
 <style scoped>
--- GETDO/src/views/HomeVue.vue
+++ GETDO/src/views/HomeVue.vue
@@ -1,19 +1,65 @@
 <script setup>
+import { ref } from "vue";
 import HeaderComponent from "../components/HeaderComponent.vue";
 import TaskComponent from "../components/TaskComponent.vue";
-import PopUpFormTaskComponent from "../components/PopUpFormTaskComponent.vue";
+
+const show_popup = ref(false);
+show_popup.value = false;
 </script>
 
 <template>
   <HeaderComponent />
   <body class="page-container">
-    <PopUpFormTaskComponent/>
+    <!-- <PopUpFormTaskComponent/> -->
+    <div class="popup-content" v-if="show_popup">
+      <form class="popup-content-form">
+        <label for="task_name">Nom de la tâche</label>
+        <input
+          class="generic-input-fields"
+          type="text"
+          name=""
+          id="task_name"
+        />
+        <label for="task_desc">Description de la tâche</label>
+        <textarea
+          name=""
+          class="generic-input-fields"
+          id="task_desc"
+        ></textarea>
+        <hr />
+        <div class="priority-content">
+          <p>priorité de la tâche:</p>
+          <label for="light">faible</label>
+          <input type="radio" name="" id="light" />
+
+          <label for="medium">moyenne</label>
+          <input type="radio" name="" id="medium" />
+
+          <label for="heavy">haute</label>
+          <input type="radio" name="" id="heavy" />
+
+          <label for="phat">élevé</label>
+          <input type="radio" name="" id="phat" />
+        </div>
+        <hr />
+        <div class="date-content">
+          <label for="date">date d'échéance </label>
+          <input class="generic-input-fields" type="date" name="" id="date" />
+        </div>
+        <hr />
+        <button class="fancyButton">Enregistré</button>
+      </form>
+    </div>
+    <!-- <PopUpFormTaskComponent/> -->
+
     <div class="task-search">
       <input type="search" class="input-fields" name="search" id="search" />
       <button class="fancyButton research-button">
         <font-awesome-icon icon="magnifying-glass" />
       </button>
-      <button class="fancyButton">Crée une nouvelle tâche</button>
+      <button class="fancyButton" @click="show_popup = true">
+        Crée une nouvelle tâche
+      </button>
     </div>
     <TaskComponent />
   </body>
