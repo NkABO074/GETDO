bf75f9b11825cb535ca89661ebb8f2869a2d6081
NkABO074 <nkabo074@gmail.com>
2024-07-21 02:33:55 +0100

    few changes

--- GETDO/src/views/HomeVue.vue
+++ GETDO/src/views/HomeVue.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import HeaderComponent from "../components/HeaderComponent.vue";
 import TaskComponent from "../components/TaskComponent.vue";
+import PopUpFormTaskComponent from "../components/PopUpFormTaskComponent.vue";
 
 const show_popup = ref(false);
 show_popup.value = false;
@@ -10,48 +11,8 @@ show_popup.value = false;
 <template>
   <HeaderComponent />
   <body class="page-container">
+     <PopUpFormTaskComponent/>
     <!-- <PopUpFormTaskComponent/> -->
-    <div class="popup-content" v-if="show_popup">
-      <form class="popup-content-form">
-        <label for="task_name">Nom de la tâche</label>
-        <input
-          class="generic-input-fields"
-          type="text"
-          name=""
-          id="task_name"
-        />
-        <label for="task_desc">Description de la tâche</label>
-        <textarea
-          name=""
-          class="generic-input-fields"
-          id="task_desc"
-        ></textarea>
-        <hr />
-        <div class="priority-content">
-          <p>priorité de la tâche:</p>
-          <label for="light">faible</label>
-          <input type="radio" name="" id="light" />
-
-          <label for="medium">moyenne</label>
-          <input type="radio" name="" id="medium" />
-
-          <label for="heavy">haute</label>
-          <input type="radio" name="" id="heavy" />
-
-          <label for="phat">élevé</label>
-          <input type="radio" name="" id="phat" />
-        </div>
-        <hr />
-        <div class="date-content">
-          <label for="date">date d'échéance </label>
-          <input class="generic-input-fields" type="date" name="" id="date" />
-        </div>
-        <hr />
-        <button class="fancyButton">Enregistré</button>
-      </form>
-    </div>
-    <!-- <PopUpFormTaskComponent/> -->
-
     <div class="task-search">
       <input type="search" class="input-fields" name="search" id="search" />
       <button class="fancyButton research-button">
@@ -66,6 +27,16 @@ show_popup.value = false;
 </template>
 
 <style scoped>
+
+.close_button {
+  background-color: red;
+  width: 24px;
+  height: 24px;
+  border-radius: 20px;
+  border: 1px solid rgb(78, 0, 0);
+  text-align: center;
+}
+
 .input-fields {
   background-color: var(--default-color);
   border: 1px solid var(--deap-gray);
@@ -93,18 +64,7 @@ show_popup.value = false;
 }
 
 /* popup css */
-.popup-content {
-  font-size: 14px;
-  background-color: var(--box-bg-color);
-  border-radius: 20px;
-  width: 50vw;
-}
 
-.popup-content-form {
-  display: flex;
-  flex-direction: column;
-  padding: 13px;
-}
 /* --- */
 
 @media only screen and (max-width: 768px) {
