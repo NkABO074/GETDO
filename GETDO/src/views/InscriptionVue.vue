50f5eb7de69ee230991f17f49d685f3a55203fe4
NkABO074 <nkabo074@gmail.com>
2024-07-29 21:33:19 +0100

    few changes

--- GETDO/src/views/InscriptionVue.vue
+++ GETDO/src/views/InscriptionVue.vue
@@ -1,5 +1,26 @@
 <script setup>
+import { ref } from 'vue'
+import { supabase } from '../lib/supabase'
 
+const loading = ref(false)
+const email = ref('')
+
+const handleLogin = async () => {
+  try {
+    loading.value = true
+    const { error } = await supabase.auth.signInWithOtp({
+      email: email.value,
+    })
+    if (error) throw error
+    alert('Check your email for the login link!')
+  } catch (error) {
+    if (error instanceof Error) {
+      alert(error.message)
+    }
+  } finally {
+    loading.value = false
+  }
+}
 </script>
 
 <template>
@@ -17,6 +38,7 @@
           name="email"
           id="email_input"
           required
+          placeholder="jonhDoe@example.plod"
           class="generic-input-fields"
         />
 
